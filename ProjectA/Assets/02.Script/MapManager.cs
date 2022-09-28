993ea64a92bb6c3b500f2057837f61b5d5deac20
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2022-09-28 20:16:17 +0900

    2022_09_28_20:16

---
+++ ProjectA/Assets/02.Script/MapManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    #region singleton
+    private static MapManager instance = null;
+
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public static MapManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+
+    public GameObject player;
+    public List<GameObject> mapList = new List<GameObject>(); //생성되는 맵의 종류를 저장하는 리스트
+    public List<float> mapSpawnPer = new List<float>(); //맵이 생성될확률을 저장하는 리스트
+
+    public delegate void CreatMapEvent();
+    public static event CreatMapEvent creatMapEvent;
+    public GameObject CreatMap(Vector2 mapPos) //새로운 맵을 만들어주는 함수
+    {
+        for (int i = 0; i < mapList.Count; i++)
+        {
+            if (Random.Range(1, 101) < mapSpawnPer[i])
+            {
+                //Instantiate(mapList[i], mapPos, Quaternion.identity);
+                return Instantiate(mapList[i], mapPos, Quaternion.identity);
+            }
+        }
+
+        return null;
+    }
+
+    public void MapReGenerate()
+    {
+        creatMapEvent();
+    }
+}
