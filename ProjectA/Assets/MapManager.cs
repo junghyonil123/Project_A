d8fe806f4ee85a057407108b64ad2976091fee4e
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2022-09-28 10:24:06 +0900

    2022_09_28_10:23 맵 랜덤생성 알고리즘

--- ProjectA/Assets/MapManager.cs
+++ ProjectA/Assets/MapManager.cs
@@ -32,19 +32,18 @@ public class MapManager : MonoBehaviour
     #endregion
 
 
+    public GameObject player;
     public List<GameObject> mapList = new List<GameObject>(); //생성되는 맵의 종류를 저장하는 리스트
     public List<float> mapSpawnPer = new List<float>(); //맵이 생성될확률을 저장하는 리스트
 
     public GameObject CreatMap(Vector2 mapPos) //새로운 맵을 만들어주는 함수
     {
-        Debug.Log("mapPos");
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < mapList.Count; i++)
         {
             if (Random.Range(1, 101) < mapSpawnPer[i])
             {
                 //Instantiate(mapList[i], mapPos, Quaternion.identity);
                 return Instantiate(mapList[i], mapPos, Quaternion.identity);
-                Debug.Log("다만들엇다");
             }
         }
 
