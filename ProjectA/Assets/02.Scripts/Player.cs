bbc01f831633b73f5ca2abe8ffe205e4166370ea
123123445 <wjdalstltm2@naver.com>
2022-09-29 14:06:52 +0900

    2022_09_29_14:06

--- ProjectA/Assets/02.Scripts/Player.cs
+++ ProjectA/Assets/02.Scripts/Player.cs
@@ -55,11 +55,6 @@ public class Player : Unit
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);   //ray에 걸리는 물체 hit에 저장
             if (hit.collider == null)   //만약 ray가 땅이없는 곳을 눌렀을때 에러 발생을 막기위한 return
                 return;
-            if(hit.collider.transform.position == transform.position + new Vector3(-3,-3) || hit.collider.transform.position == transform.position + new Vector3(-3,3) || hit.collider.transform.position == transform.position + new Vector3(3, 3) || hit.collider.transform.position == transform.position + new Vector3(3, -3))
-            {   //대각선 이동을 잠금
-                Debug.Log(hit.collider.transform.position);
-                return;
-            }
             Debug.Log(hit.collider.gameObject);
             target = hit.collider.transform;    //ray에 잡힌 물체를 target변수에 집어넣음
             if(transform.position != target.transform.position)     //Player가 서있는 땅을 눌러도 activePoint 소모 안하도록 막음
