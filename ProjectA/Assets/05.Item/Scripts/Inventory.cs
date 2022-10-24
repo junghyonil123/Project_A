aa423cf5d349f26de1ae05908b2a1601b6375048
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2022-10-24 12:12:17 +0900

    2022_10_24_12:12 아이템DB저장

--- ProjectA/Assets/05.Item/Scripts/Inventory.cs
+++ ProjectA/Assets/05.Item/Scripts/Inventory.cs
@@ -49,7 +49,7 @@ public class Inventory : MonoBehaviour
     }
     #endregion
 
-    public List<GameObject> inventory = new List<GameObject>();
+    public List<GameObject> inventoryList = new List<GameObject>();
 
 
     public GameObject equipmentWindow;
@@ -68,7 +68,7 @@ public class Inventory : MonoBehaviour
 
     public void GetItem(Item item)
     {
-        foreach (GameObject slot in inventory)
+        foreach (GameObject slot in inventoryList)
         {
             if (slot.GetComponent<Slot>().item == null)
             {
